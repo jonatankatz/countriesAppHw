1ba358554b1e1f27797fdb288628dc4f9998c651
jonatan katz <jonatankatz4@gmail.com>
2019-08-12 01:47:31 +0300

    arrange the code to merge

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 //slider arrows functions
 let startSlide = function() {
     var prevArrow = document.getElementsByClassName('slideshow-left-control')[0];
@@ -10,90 +8,122 @@ let startSlide = function() {
 };
 
 //find all country 
-const findCountry = function findCountry(){
-const result = countries.filter((country) => {
-    let findInput = document.querySelector("#findInput").value.toString()
-    let currentCountry = JSON.stringify(country.name).toLowerCase()
-    return (currentCountry.includes(findInput))
-})
-return result
+const findCountry = function findCountry() {
+    const result = countries.filter((country) => {
+        let findInput = document.querySelector("#findInput").value.toString()
+        let currentCountry = JSON.stringify(country.name).toLowerCase()
+        return (currentCountry.includes(findInput))
+    })
+    return result
 }
 //find One
-const findOneFunction = function findOne(){
+const findOneFunction = function findOne() {
     const oneCountry = countries.filter((country) => {
         let findInput = JSON.stringify(document.querySelector("#findInput").value)
         let currentCountry = JSON.stringify(country.name).toLowerCase()
-        const { name,flag } = country;
+        const {
+            name,
+            flag
+        } = country;
         console.log(currentCountry)
         console.log(findInput)
-        if (currentCountry === findInput) return {countryName: name,flag}
-})
-return oneCountry
+        if (currentCountry === findInput) return {
+            countryName: name,
+            flag
+        }
+    })
+    return oneCountry
 }
 //clean & show
-const cleanDisplay = function cleanDisplay(){
+const cleanDisplay = function cleanDisplay() {
     $("#imgSlider").html("")
     $("#indicator").html("")
 }
 
 
 ///  find all function
-const flagAndName = function flagAndName(countries){
-    const countryFlags = countries.map((country ,index) => {
-    const {name,flag} = country;
-    return{name,flag}
-})
-countryFlags.map((flag,index) => {
-    if(countryFlags.length < 2){{$(".slideshow-arrow").hide()}}
-    else{$(".slideshow-arrow").show()}
-    let countryName = flag.name
-    let flagImg = flag.flag
-    let inactive = "inactive"
-    let indicator = "inactive-indicator"
-    if(index<1) {inactive = "";  
-    indicator = "";}
-    
-$("#indicator").append(` <li class='slideshow-indicator ${indicator}'></li>`)
-$("#imgSlider").append(`<div class='che-slideshow-slide ${inactive}'><img src='${flagImg}'><div class='che-slideshow-caption'>${countryName}</div>`) 
+const flagAndName = function flagAndName(countries) {
+    const countryFlags = countries.map((country, index) => {
+        const {
+            name,
+            flag
+        } = country;
+        return {
+            name,
+            flag
+        }
+    })
+    countryFlags.map((flag, index) => {
+        if (countryFlags.length < 2) {
+            {
+                $(".slideshow-arrow").hide()
+            }
+        } else {
+            $(".slideshow-arrow").show()
+        }
+        let countryName = flag.name
+        let flagImg = flag.flag
+        let inactive = "inactive"
+        let indicator = "inactive-indicator"
+        if (index < 1) {
+            inactive = "";
+            indicator = "";
+        }
 
-})
+        $("#indicator").append(` <li class='slideshow-indicator ${indicator}'></li>`)
+        $("#imgSlider").append(`<div class='che-slideshow-slide ${inactive}'><img src='${flagImg}'><div class='che-slideshow-caption'>${countryName}</div>`)
+
+    })
 }
 
 
 ///  find capital function
-const flagAndCapital = function flagAndCapital(countries){
-    const countryFlags = countries.map((country ,index) => {
-    const {capital,flag} = country;
-    return{capital,flag}
-})
-countryFlags.map((flag,index) => {
-    if(countryFlags.length < 2){{$(".slideshow-arrow").hide()}}
-    else{$(".slideshow-arrow").show()}
-    let countryCapital = flag.capital
-    let flagImg = flag.flag
-    let inactive = "inactive"
-    let indicator = "inactive-indicator"
-    if(index<1) {inactive = "";  
-    indicator = "";}
-    
-$("#indicator").append(` <li class='slideshow-indicator ${indicator}'></li>`)
-$("#imgSlider").append(`<div class='che-slideshow-slide ${inactive}'><img src='${flagImg}'> <div class="che-slideshow-caption capital"><h3>${countryCapital}</h3></div>`)
+const flagAndCapital = function flagAndCapital(countries) {
+    const countryFlags = countries.map((country, index) => {
+        const {
+            capital,
+            flag
+        } = country;
+        return {
+            capital,
+            flag
+        }
+    })
+    countryFlags.map((flag, index) => {
+        if (countryFlags.length < 2) {
+            {
+                $(".slideshow-arrow").hide()
+            }
+        } else {
+            $(".slideshow-arrow").show()
+        }
+        let countryCapital = flag.capital
+        let flagImg = flag.flag
+        let inactive = "inactive"
+        let indicator = "inactive-indicator"
+        if (index < 1) {
+            inactive = "";
+            indicator = "";
+        }
 
-})
+        $("#indicator").append(` <li class='slideshow-indicator ${indicator}'></li>`)
+        $("#imgSlider").append(`<div class='che-slideshow-slide ${inactive}'><img src='${flagImg}'> <div class="che-slideshow-caption capital"><h3>${countryCapital}</h3></div>`)
+
+    })
 }
 //on click find all 
 const findAll = $("#findAll")
-findAll.on("click",function(){
+findAll.on("click", function() {
     cleanDisplay()
-   let countries = findCountry()
+    let countries = findCountry()
     flagAndName(countries)
     startSlide()
     document.querySelector('.che-slideshow').style.visibility = "visible"
-    })
+})
 
 //on click find one 
 const findOne = $("#findOne")
-findOne.on("click",function(){
+findOne.on("click", function() {
     console.log('clicked')
     cleanDisplay()
     let countries = findOneFunction()
@@ -104,52 +134,61 @@ findOne.on("click",function(){
 
 //on click find Capital city
 const findCapital = $("#findCapital")
-findCapital.on("click",function(){
+findCapital.on("click", function() {
     cleanDisplay()
     let countries = findCountry()
     flagAndCapital(countries)
     startSlide()
     document.querySelector('.che-slideshow').style.visibility = "visible"
 
+})
+// currencies drop down function
+const currenciesDd = function currenciesDd(countries) {
+    $("#CountriesDropD").on("input", function() {
+        $("#CurrenciesDropD").html("")
+        let countryVal = JSON.stringify(document.querySelector("#CountriesDropD").value).toLowerCase()
+        let findCurrency = countries.filter((country) => {
+            let currentCountry = JSON.stringify(country.name).toLowerCase()
+            const {
+                currencies
+            } = country;
+            if (currentCountry === countryVal) return {
+                countryName: currencies
+            }
         })
-const currenciesDd = function currenciesDd(countries){
-        $("#CountriesDropD").on("input",function(){
-            $("#CurrenciesDropD").html("")
-            let countryVal = JSON.stringify(document.querySelector("#CountriesDropD").value).toLowerCase()
-            let findCurrency = countries.filter((country) => {
-                let currentCountry = JSON.stringify(country.name).toLowerCase()
-                const {currencies} = country;
-                if (currentCountry === countryVal) return {countryName: currencies}
+        let currencies = findCurrency.map((country, index) => {
+            return country.currencies
         })
-          let currencies =  findCurrency.map((country,index) =>{
-                return country.currencies 
+        currencies.map((currency, index) => {
+            currency.map((currentCurrency, index) => {
+                let currencyName = currentCurrency.name
+                $("#CurrenciesDropD").append($(`<option></option>`).attr("value", currencyName).text(currencyName));
             })
-            currencies.map((currency,index)=>{
-                currency.map((currentCurrency,index)=>{
-                    let currencyName = currentCurrency.name
-                    $("#CurrenciesDropD").append($(`<option></option>`).attr("value",currencyName).text(currencyName));
-                })
-            })
         })
+    })
 }
-
-const fillCdd = function fillCdd(countries){
-    countries.map((country,index) => {
+// Countries drop down function
+const fillCdd = function fillCdd(countries) {
+    countries.map((country, index) => {
         let countryName = country.name
-        $("#CountriesDropD").append($(`<option></option>`).attr("value",countryName).text(countryName)); 
+        $("#CountriesDropD").append($(`<option></option>`).attr("value", countryName).text(countryName));
 
 
-})
+    })
 }
-
-const countriesDd = function countriesDd(){
-const countriesForDd = countries.map((country ,index) => {
-    const {name,currencies} = country;
-    return{name,currencies}
-})
-fillCdd(countriesForDd)
-let currencies = currenciesDd(countriesForDd)
+// init function countries drop down fill
+const countriesDd = function countriesDd() {
+    const countriesForDd = countries.map((country, index) => {
+        const {
+            name,
+            currencies
+        } = country;
+        return {
+            name,
+            currencies
+        }
+    })
+    fillCdd(countriesForDd)
+    let currencies = currenciesDd(countriesForDd)
 }
 countriesDd()
-
-
